5bb3d106d088805e99924223b8ee4451926be00d
280125706@qq.com <280125706@qq.com>
2015-09-09 13:36:04 +0800

    aa

--- src/Demo.java
+++ src/Demo.java
@@ -1,7 +1,8 @@
 
 public class Demo {  
     public static void main(String[] args) {
-    	System.out.println("2222");	   
+    	System.out.println("2222");	     
+    	System.out.println("222222");	   
 	System.out.println("222222");	   
 	}
 } 
